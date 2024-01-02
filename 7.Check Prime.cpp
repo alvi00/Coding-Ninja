83355461757c66fb6703e29e001e9fcc0600ec73
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-02 14:15:29 +0600

    Create 7.Check Prime.cpp

---
+++ 7.Check Prime.cpp
@@ -0,0 +1,17 @@
+bool isPrime(int n)
+{
+	int answer=0;
+	for(int i=2;i<n;i++){
+		if(n%i==0){
+			answer++;
+		}
+	}
+	 if(n==1){
+        return false;
+    }
+	else if(answer>0){
+		return false;
+	}else{
+		return true;
+	}
+}
