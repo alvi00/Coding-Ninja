5f92091bf763de2c69fbe95104d268ac95b82baf
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-03 14:00:04 +0600

    Update 7.Check Prime.cpp

--- 7.Check Prime.cpp
+++ 7.Check Prime.cpp
@@ -15,3 +15,23 @@ bool isPrime(int n)
 		return true;
 	}
 }
+
+//here the time complexity is O(n)
+
+bool isPrime(int n)
+{
+	int answer=0;
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0){
+			answer++;
+		}
+	}
+	 if(n==1){
+        return false;
+    }
+	else if(answer>0){
+		return false;
+	}else{
+		return true;
+	}
+}
