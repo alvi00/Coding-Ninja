0b96beddec7102130aa29b83eeb64dd054c8af9c
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2024-01-10 03:01:00 +0600

    Update 9_16.Insertion Sort.cpp

--- 9_16.Insertion Sort.cpp
+++ 9_16.Insertion Sort.cpp
@@ -1,29 +1,13 @@
 #include <bits/stdc++.h>
-using namespace std;
-int main()
+void insertionSort(int arr[], int n)
 {
-    cout << "Enter a size of the array" << endl;
-    int n;
-    cin >> n;
-    int arr[n];
-    cout << "Give the elements" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        int j = i;
-        while (j > 0 && arr[j - 1] > arr[j])
-        {
-            swap(arr[j - 1], arr[j]);
-            j--;
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i];
-    }
+   for(int i=0;i<n;i++){
+       int j=i;
+       while(j>0&&arr[j-1]>arr[j]){
+           int temp=arr[j-1];
+           arr[j-1]=arr[j];
+           arr[j]=temp;
+           j--;
+       }
+   }
 }
